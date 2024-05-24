97ea0209b3bdd6f59ddff368918c6c35ccaa962d
whougie <whougie.c.lo@gmail.com>
2024-05-24 00:19:05 -0500

    Adding the Sequelize and Models functionality

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  onDelete: "SET NULL"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: 'product_tag',
+    unique: false
+  },
+  as: 'product_with_tags',
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: 'product_tag',
+    unique: false
+  },
+  as: 'tag_with_products',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync({ force: false })
+.then( () => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+})
+
+// Be able to connect using the .env file
+//XX Be able to run the schema file
+// Be able to run the seeds
+// Setup Express to use Sequelize
+// Setup Sequelize to use the Models with correct attributes
+// The routes for products or tags should work
+// Able to used CRUD for routes
